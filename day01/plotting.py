44159339cec3b5c40f8af133721d78eb307635f1
01-AlstonAlvares <112925234+01-AlstonAlvares@users.noreply.github.com>
2023-12-27 05:29:30 -0800

    Add files via upload

---
+++ day01/plotting.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+x = [1,2,3,4]
+y= [1500,1200,1100,1800]
+plt.plot(x,y)
+legends = ["january","februay","March","April"]
+plt.xticks(x,legends)
+plt.bar
+plt.ylabel("sales")
+plt.title('Monthly sales')
+plt.show()
